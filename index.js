c1670105564f98452f1b19c7a845194cae99cf9a
Aaron Rodi <metalhead5209@gmail.com>
2022-11-17 08:20:22 -0600

    added some comments

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-const express = require('express');
-require('dotenv').config();
-const app = express();
-const SerpApi = require('google-search-results-nodejs');
+const express = require("express");
+require("dotenv").config();
+const app = express(); 
 const key = process.env.API_KEY;
-const search = new SerpApi.GoogleSearch(key)
 
-
+// imports SerpApi
+const SerpApi = require("google-search-results-nodejs");
+const search = new SerpApi.GoogleSearch(key);
 
 const PORT = process.env.PORT;
 
-app.use(express.json())
-app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
-})
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
 
-app.post('/api', (req, res) => {
-    console.log(req.body);
-    const params = {
-        q: `${req.body.name} toy`,
-        tbm: 'isch',
-        ijn: 0,
-        safe: 'active',
-        star: 1,
-        num: 1
-    }
-    const callback = (data) => {
-        const IMAGE = data['images_results'][0]['original']
-        console.log(IMAGE);
-    }
-    search.json(params, callback)
-    res.json({
-        message: "Data Recieved!"
-    })
-})
-
+app.post("/api", (req, res) => {
+  console.log(req.body);
+//  SerpApi takes in two arguments. Fist are the parameters of your search criteria eg. q: query, tbm: data type, safe: safe search.
+// The second argument is the callback function which is where you define what to do with that data.
+  const params = {
+    q: `${req.body.name} toy`,
+    tbm: "isch",
+    ijn: 0,
+    safe: "active",
+    star: 1,
+    num: 1,
+  };
+//   For the callback function, I just intialized a variable as the data and congole logged it. Follow the link of the logged data and it should bring you to a picure of what you searched for on the frontend. 
+  const callback = (data) => {
+    const IMAGE = data["images_results"][0]["original"];
+    console.log(IMAGE);
+  };
+  search.json(params, callback);
+  res.json({
+    message: "Data Recieved!",
+  });
+});
 
 app.listen(PORT, () => {
-    console.log('On port 3000')
-})
+  console.log("On port 3000");
+});
--- public/main.js
+++ public/main.js
@@ -3,15 +3,20 @@ const btn = document.querySelector("button");
 const link = "https://swapi.dev/api/people/?search=";
 
 const charSearch = async (char) => {
+  // set param as input value
   char = input.value;
   try {
+    // queries the Star Wars API which returns an object populated with the query data
     await fetch(`${link}${char}`)
       .then((res) => res.json())
       .then((data) => {
+        // console logs just the name of the queried result
         console.log(data.results[0].name);
+        // initializes an object with the result of your query as the value.
         let obj = {
           name: data.results[0].name,
         };
+        // Uses fetch to post the object we just created using the fetched Star Wars data to the backend with a /api endpoint
         fetch("/api", {
           method: "POST",
           headers: {
@@ -19,6 +24,7 @@ const charSearch = async (char) => {
           },
           body: JSON.stringify(obj),
         })
+        // Sends back confirmation when data is received on backend.
           .then((r) => r.json())
           .then((res) => console.log(res));
         console.log(obj);
@@ -29,6 +35,7 @@ const charSearch = async (char) => {
 };
 
 btn.addEventListener("click", () => {
+  // if the input is an empty string or less than 3 characters, alert
   if (input.value == "" || input.selectionEnd < 3) {
     alert("Please Enter Name");
   } else {
