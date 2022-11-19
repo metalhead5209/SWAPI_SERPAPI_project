8d794170a7a79d25b172853f1704cbeb1b7bea45
Aaron Rodi <metalhead5209@gmail.com>
2022-11-19 14:05:14 -0600

    core functionality somewhat complete. ability to search char and render pic onto screen

--- public/main.js
+++ public/main.js
@@ -8,18 +8,18 @@ const charSearch = async (char) => {
   try {
     // queries the Star Wars API which returns an object populated with the query data
 
-
     await fetch(`${link}${char}`)
       .then((res) => res.json())
-      .then((data) => {
+      .then(async (data) => {
         // console logs just the name of the queried result
         console.log(data.results[0].name);
         // initializes an object with the result of your query as the value.
         let obj = {
           name: data.results[0].name,
         };
+
         // Uses fetch to post the object we just created using the fetched Star Wars data to the backend with a /api endpoint
-        fetch("/api", {
+        await fetch("/api", {
           method: "POST",
           headers: {
             "Content-type": "application/json",
@@ -30,21 +30,21 @@ const charSearch = async (char) => {
           .then((r) => r.json())
           .then((res) => console.log(res));
         console.log(obj);
+        if(data) {
+          setTimeout(() => {
+          location.reload();
+        }, 1000);
+        }
+        
       });
   } catch (error) {
     console.error(error, "I find your lack of SW knowledge disturbing");
+    alert("I find your lack of SW knowledge disturbing");
   }
   input.value = "";
-
-  setTimeout(() => {
-    location.reload()
-  }, 1000)
- 
 };
 
-
-
-btn.addEventListener("click",  () => {
+btn.addEventListener("click", () => {
   // if the input is an empty string or less than 3 characters, alert
   if (input.value == "" || input.selectionEnd < 3) {
     alert("Please Enter Name");
@@ -52,4 +52,3 @@ btn.addEventListener("click",  () => {
     charSearch();
   }
 });
-
