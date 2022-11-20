e365025836e786c0ce0c5ff023a07733e9ae668e
Aaron Rodi <metalhead5209@gmail.com>
2022-11-20 06:09:37 -0600

    SerpApi has a 100 req limit. This branch is for when Serp is inactive. Serp logic commented out and just returns char object

--- api.js
+++ api.js
@@ -14,8 +14,8 @@ app.engine("hbs", handleBars.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 // imports SerpApi
-const SerpApi = require("google-search-results-nodejs");
-const search = new SerpApi.GoogleSearch(key);
+// const SerpApi = require("google-search-results-nodejs");
+// const search = new SerpApi.GoogleSearch(key);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,29 +27,41 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api", (req, res) => {
-  // console.log(req.body);
+
+  console.log(req.body);
+  
   //  SerpApi takes in two arguments. Fist are the parameters of your search criteria eg. q: query, tbm: data type, safe: safe search.
   // The second argument is the callback function which is where you define what to do with that data.
-  const params = {
-    q: `${req.body.results[0].name} toy`,
-    tbm: "isch",
-    ijn: 0,
-    safe: "active",
-    star: 1,
-    num: 1,
-  };
-  console.log(req.body.results[0].name)
+
+
+  // const params = {
+  //   q: `${req.body.results[0].name} toy`,
+  //   tbm: "isch",
+  //   ijn: 0,
+  //   safe: "active",
+  //   star: 1,
+  //   num: 1,
+  // };
+
+
+  // console.log(req.body.results[0].name)
+
+
   //   For the callback function, I just intialized a variable as the data and congole logged it. Follow the link of the logged data and it should bring you to a picure of what you searched for on the frontend.
-  const callback = (data) => {
-    const IMAGE = {
-      id: Math.floor(Math.random() * 200) + 1,
-      url: data["images_results"][Math.floor(Math.random() * 20)]["original"],
-    };
-    console.log(IMAGE);
-    DB.push(IMAGE);
-    // console.log(DB);
-  };
-  search.json(params, callback);
+
+
+
+  // const callback = (data) => {
+  //   const IMAGE = {
+  //     id: Math.floor(Math.random() * 200) + 1,
+  //     url: data["images_results"][Math.floor(Math.random() * 20)]["original"],
+  //   };
+  //   console.log(IMAGE);
+  //   DB.push(IMAGE);  
+  // };
+  // search.json(params, callback);
+  
+  
   res.json({
     message: "Hello There",
   });
